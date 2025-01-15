c464559ff9c09bed2b8d795b38a168e8b71f2e8f
Ramiro Berrelleza <ramiro@okteto.com>
2025-01-15 15:49:22 -0800

    celebrate Jan 15

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8080;
 
-app.get('/', function (req, res) {
-	res.send('Hola mundo!');
-})
+app.get("/", function (req, res) {
+  res.send("Hola mundo Jan 15!");
+});
 
 app.listen(PORT, function () {
-  console.log('Started hola-mundo server on %d', PORT);
-})
+  console.log("Started hola-mundo server on %d", PORT);
+});
